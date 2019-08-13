b7ba3c614dbcc39f4903c3327593ce63e99962bb
Gladson <gladsonsouza@live.com>
2019-08-13 07:54:31 -0400

    Cadastro com Firebase realizado :v

--- app/src/main/java/com/gmmp/easylearn/activity/DestaquesActivity.java
+++ app/src/main/java/com/gmmp/easylearn/activity/DestaquesActivity.java
@@ -1,13 +1,15 @@
-package com.gmmp.easylearn;
+package com.gmmp.easylearn.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.gmmp.easylearn.R;
+
+public class DestaquesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_destaques);
     }
 }
