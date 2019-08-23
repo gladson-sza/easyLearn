c1aa08c45e79979c19dc27118e3a22e943b98cc0
Paulo <ribeiro.paulotm@gmail.com>
2019-08-23 17:37:06 -0400

    Disciplinas e Meus cursos

--- app/src/main/java/com/gmmp/easylearn/fragment/DisciplinasFragment.kt
+++ app/src/main/java/com/gmmp/easylearn/fragment/DisciplinasFragment.kt
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 import com.gmmp.easylearn.R
 
@@ -13,13 +15,37 @@ import com.gmmp.easylearn.R
 /**
  * A simple [Fragment] subclass.
  */
-class DisciplinasFragment : Fragment() {
+class DisciplinasFragment : Fragment()  {
 
+    private lateinit var listView: ListView
+    private var listDisciplinas = arrayListOf<String>()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_disciplinas, container, false)
+        val view = inflater.inflate(R.layout.fragment_disciplinas, container, false)
+        inicializar(view)
+        return view
+    }
+
+    fun inicializar(view: View){
+
+        listDisciplinas = inserirDisciplinas();
+
+        listView = view.findViewById<ListView>(R.id.listDisciplinas)
+
+        val listItems = inserirDisciplinas()
+// 4
+        val adapter = ArrayAdapter(activity, android.R.layout.simple_list_item_1, listItems)
+        listView.adapter = adapter
+
+    }
+
+    fun inserirDisciplinas(): ArrayList<String>{
+        var dados = arrayListOf<String>()
+        dados.addAll(listOf("Biologia", "Filosofia", "Física", "Geografia", "História", "Linguagem de Porgramação", "Línguas Estrangeiras", "Matemática", "Português", "Química"))
+
+        return  dados
     }
 
 }
