c7d21d606aa50d502c9568f5b4422f6de9812b64
Gladson <gladsonsouza@live.com>
2019-08-27 18:05:25 -0400

    Parte da Configuração do Apapter de Mensagens implementada

---
+++ app/src/main/java/com/gmmp/easylearn/adapter/MensagensAdapter.kt
@@ -0,0 +1,47 @@
+package com.gmmp.easylearn.adapter
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+import com.gmmp.easylearn.R
+import com.gmmp.easylearn.model.Mensagen
+
+import de.hdodenhof.circleimageview.CircleImageView
+
+class MensagensAdapter(private val mensagenList: List<Mensagen>, private val context: Context) : RecyclerView.Adapter<MensagensAdapter.MyViewHolder>() {
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): MyViewHolder {
+        val itemList = LayoutInflater.from(viewGroup.context)
+                .inflate(R.layout.adapter_mensagens, viewGroup, false)
+
+        return MyViewHolder(itemList)
+    }
+
+    override fun onBindViewHolder(myViewHolder: MyViewHolder, i: Int) {
+        val (id, titulo, descricao, dataAtividade) = mensagenList[i]
+
+    }
+
+    override fun getItemCount(): Int {
+        return mensagenList.size
+    }
+
+    inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        private val thumbCanal: CircleImageView
+        private val textViewNomeCanal: TextView
+        private val textViewAtividadeCanal: TextView
+        private val textViewDataAtividade: TextView
+
+        init {
+            thumbCanal = itemView.findViewById(R.id.circleImageViewMensagemThumbCanal)
+            textViewNomeCanal = itemView.findViewById(R.id.textViewMensagemNomeCanal)
+            textViewAtividadeCanal = itemView.findViewById(R.id.textViewMensagemAtividadeCanal)
+            textViewDataAtividade = itemView.findViewById(R.id.textViewMensagemDataAtividadeCanal)
+        }
+    }
+}
---
+++ app/src/main/java/com/gmmp/easylearn/model/Mensagen.kt
@@ -0,0 +1,5 @@
+package com.gmmp.easylearn.model
+
+import java.util.*
+
+data class Mensagen(var id: String, var titulo: String, var descricao: String, var dataAtividade: Date)
