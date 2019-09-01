3b100ab2d30d37caabe1dd4c4036fb55f78ee394
Gladson <gladsonsouza@live.com>
2019-09-01 11:40:09 -0400

    Adicionado construtor secundário para o Usuário

--- app/src/main/java/com/gmmp/easylearn/model/Usuario.kt
+++ app/src/main/java/com/gmmp/easylearn/model/Usuario.kt
@@ -1,5 +1,17 @@
 package com.gmmp.easylearn.model
 
-data class Usuario(var id: String, var nome: String,
-                   var email: String, var senha: String,
-                   var urlPerfil: String, var urlWallpaper: String)
+data class Usuario(var id: String, var nome: String, var email: String, var senha: String) {
+
+    var urlPerfil: String
+    var urlWallpaper: String
+
+    init {
+        urlPerfil = ""
+        urlWallpaper = ""
+    }
+
+    constructor (id: String, nome: String, email: String, senha: String, urlPerfil: String, urlWallpaper: String) : this(id, nome, email, senha) {
+        this.urlWallpaper = urlWallpaper
+        this.urlPerfil = urlPerfil
+    }
+}
