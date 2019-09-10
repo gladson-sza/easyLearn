24c99f360e6a28b60e9e4084cfb89218bdfe9b5d
Gladson <gladsonsouza@live.com>
2019-09-10 18:28:17 -0400

    Layout do botão do google, menus de configurações de conta com funções implementadas

--- app/src/main/java/com/gmmp/easylearn/activity/LoginActivity.kt
+++ app/src/main/java/com/gmmp/easylearn/activity/LoginActivity.kt
@@ -4,23 +4,29 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import com.gmmp.easylearn.R
-import com.gmmp.easylearn.fragment.DestaquesFragment
 import com.gmmp.easylearn.model.ViewDialog
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
-class LoginActivity : AppCompatActivity(){
+class LoginActivity : AppCompatActivity() {
 
-    private var btnIniciarSessao: Button? = null
-    private var txtCadastro: TextView? = null
-    private var editEmail: EditText? = null
-    private var editSenha: EditText? = null
-    private var firebaseAuth: FirebaseAuth? = null
-    private var viewDialog : ViewDialog? = null
+    private val RC_SIGN_IN = 100
+
+    private lateinit var btnIniciarSessao: Button
+    private lateinit var txtCadastro: TextView
+    private lateinit var editEmail: EditText
+    private lateinit var editSenha: EditText
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var viewDialog: ViewDialog
+    private lateinit var btnGoogle: LinearLayout
+    private lateinit var googleSignInClient: GoogleSignInClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,16 +45,16 @@ class LoginActivity : AppCompatActivity(){
         supportActionBar?.hide()
         //Inicializa o botão
         btnIniciarSessao = findViewById(R.id.buttonEntrar)
-        btnIniciarSessao!!.setOnClickListener {
+        btnIniciarSessao.setOnClickListener {
 
-            val email = editEmail!!.text.toString()
-            val senha = editSenha!!.text.toString()
+            val email = editEmail.text.toString()
+            val senha = editSenha.text.toString()
 
             if (email.isNotEmpty() && senha.isNotEmpty()) {
-                viewDialog!!.showDialog("Autenticando", "Por favor, aguarde enquanto validamos os dados")
+                viewDialog.showDialog("Autenticando", "Por favor, aguarde enquanto validamos os dados")
                 firebaseAuth = FirebaseAuth.getInstance()
-                firebaseAuth!!.signInWithEmailAndPassword(email, senha).addOnCompleteListener(this) { task ->
-                    viewDialog!!.hideDialog()
+                firebaseAuth.signInWithEmailAndPassword(email, senha).addOnCompleteListener(this) { task ->
+                    viewDialog.hideDialog()
                     if (task.isSuccessful) {
                         val intent = Intent(this@LoginActivity, NavegacaoActivity::class.java)
                         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
@@ -68,5 +74,52 @@ class LoginActivity : AppCompatActivity(){
         txtCadastro!!.setOnClickListener(View.OnClickListener {
             startActivity(Intent(applicationContext, CadastroActivity::class.java))
         })
+
+        // Configura o Token do Google
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        // Configura clique do botão do Google
+        btnGoogle = findViewById(R.id.buttonLoginGoogle)
+        btnGoogle.setOnClickListener {
+            Toast.makeText(applicationContext, "AAA", Toast.LENGTH_SHORT)
+            val signInIntent = googleSignInClient.signInIntent
+            startActivityForResult(signInIntent, RC_SIGN_IN)
+        }
+
     }
+
+    private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(acct.idToken, null)
+        firebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        val intent = Intent(this@LoginActivity, NavegacaoActivity::class.java)
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(applicationContext, "Falha ao tentar autenticar com o google", Toast.LENGTH_SHORT)
+                    }
+                }
+    }
+
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)
+                firebaseAuthWithGoogle(account!!)
+            } catch (e: ApiException) {
+                Toast.makeText(applicationContext, "Falha ao tentar autenticar com o google", Toast.LENGTH_SHORT)
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/gmmp/easylearn/fragment/MinhaContaFragment.kt
+++ app/src/main/java/com/gmmp/easylearn/fragment/MinhaContaFragment.kt
@@ -3,37 +3,20 @@ package com.gmmp.easylearn.fragment
 import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
-import android.widget.TextView
-
 import com.gmmp.easylearn.R
 import com.gmmp.easylearn.activity.LoginActivity
 import com.gmmp.easylearn.activity.MeuCanalActivity
-import com.gmmp.easylearn.activity.NovoCursoActivity
-import com.gmmp.easylearn.adapter.CursosDisponibilizadosAdapter
-import com.gmmp.easylearn.model.Curso
-import com.gmmp.easylearn.model.Usuario
-import com.gmmp.easylearn.model.ViewDialog
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
-import kotlinx.android.synthetic.main.fragment_minha_conta.*
 import kotlinx.android.synthetic.main.fragment_minha_conta.view.*
-import kotlin.collections.ArrayList
 
 
 /**
  * A simple [Fragment] subclass.
  */
 class MinhaContaFragment : Fragment() {
-
-    private var listCursos: ArrayList<Curso> = ArrayList()
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -46,22 +29,19 @@ class MinhaContaFragment : Fragment() {
         return view
     }
 
-    fun iniciar(view: View){
-
-        var meuCanal = view.findViewById(R.id.meu_canal) as LinearLayout
-
-        meuCanal.setOnClickListener {
-            startActivity(Intent(activity, MeuCanalActivity::class.java))
-        }
-
-        var editarConta = view.findViewById(R.id.editar_conta) as LinearLayout
-
-        editarConta.setOnClickListener {
+    private fun iniciar(view: View) {
+        view.menuMeuCanal.setOnClickListener { startActivity(Intent(activity, MeuCanalActivity::class.java)) }
+        view.menuEditarConta.setOnClickListener {
             val transaction = activity?.supportFragmentManager?.beginTransaction()
             transaction?.replace(R.id.frameContainer, EditarPerfilFragment())
             transaction?.commit()
         }
-    }
+        view.menuSair.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            activity?.startActivity(Intent(context, LoginActivity::class.java))
+            activity?.finish()
+        }
 
+    }
 
 }
