ec5949340aefe7fa8195e39705e283e406698821
Paulo <ribeiro.paulotm@gmail.com>
2019-09-11 16:35:44 -0400

    Compartilhar easyCash

--- app/src/main/java/com/gmmp/easylearn/fragment/MinhaContaFragment.kt
+++ app/src/main/java/com/gmmp/easylearn/fragment/MinhaContaFragment.kt
@@ -9,7 +9,14 @@ import android.view.ViewGroup
 import com.gmmp.easylearn.R
 import com.gmmp.easylearn.activity.LoginActivity
 import com.gmmp.easylearn.activity.MeuCanalActivity
+import com.gmmp.easylearn.model.Usuario
+import com.gmmp.easylearn.model.ViewDialog
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import kotlinx.android.synthetic.main.fragment_editar_perfil.view.*
 import kotlinx.android.synthetic.main.fragment_minha_conta.view.*
 
 
@@ -17,7 +24,6 @@ import kotlinx.android.synthetic.main.fragment_minha_conta.view.*
  * A simple [Fragment] subclass.
  */
 class MinhaContaFragment : Fragment() {
-
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -31,16 +37,51 @@ class MinhaContaFragment : Fragment() {
 
     private fun iniciar(view: View) {
         view.menuMeuCanal.setOnClickListener { startActivity(Intent(activity, MeuCanalActivity::class.java)) }
+
         view.menuEditarConta.setOnClickListener {
             val transaction = activity?.supportFragmentManager?.beginTransaction()
             transaction?.replace(R.id.frameContainer, EditarPerfilFragment())
             transaction?.commit()
         }
+
+        // Incializa os compoenentes da tela
+        val viewDialog = ViewDialog(activity)
+        var id = "Erro ao obter o id"
+
+        // Carrega as Informações do Usuário no Seu Perfil
+        viewDialog.showDialog("Aguarde", "Obtendo informações de nossos servidores")
+
+        val auth = FirebaseAuth.getInstance().currentUser
+        val usuario = FirebaseDatabase.getInstance().reference.child("usuarios").child(auth!!.uid)
+
+        usuario.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val u = dataSnapshot.getValue(Usuario::class.java)
+                id = u!!.id
+                // Fecha o Dialog após carregar os dados
+                viewDialog.hideDialog()
+            }
+            override fun onCancelled(p0: DatabaseError) {
+            }
+        })
+
+        view.menuCompatilharEasyCash.setOnClickListener {
+            val myIntent = Intent(Intent.ACTION_SEND)
+            myIntent.type = "type/plain"
+            val shareBody = "EasyLearning: Baixe e ganhe promoções na compra de qualquer curso usando o código abaixo, não perca!"
+            val shareSub = id
+            myIntent.putExtra(Intent.EXTRA_SUBJECT, shareBody)
+            myIntent.putExtra(Intent.EXTRA_TEXT, shareSub)
+            startActivity(Intent.createChooser(myIntent, "Compartilhar EasyCash"))
+        }
+
         view.menuSair.setOnClickListener {
             FirebaseAuth.getInstance().signOut()
             activity?.startActivity(Intent(context, LoginActivity::class.java))
             activity?.finish()
         }
+
+
 
     }
 
