5a3b7de6f51cabb0023303a90cfd204d17e4f93b
Paulo <ribeiro.paulotm@gmail.com>
2019-10-10 11:52:09 -0400

    Meu canal e ajuste dos icons

--- app/src/main/java/com/gmmp/easylearn/fragment/MeuCanalFragment.kt
+++ app/src/main/java/com/gmmp/easylearn/fragment/MeuCanalFragment.kt
@@ -1,20 +1,29 @@
 package com.gmmp.easylearn.fragment
 
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AbsListView
+import com.bumptech.glide.Glide
 
 import com.gmmp.easylearn.R
-import com.gmmp.easylearn.adapter.MensagensAdapter
-import com.gmmp.easylearn.model.Mensagen
-import java.util.*
-import kotlin.collections.ArrayList
+import com.gmmp.easylearn.activity.MainActivity
+import com.gmmp.easylearn.activity.NovoCursoActivity
+import com.gmmp.easylearn.adapter.CursosAdapter
+import com.gmmp.easylearn.dialog.ViewDialog
+import com.gmmp.easylearn.model.Curso
+import com.gmmp.easylearn.model.Usuario
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import kotlinx.android.synthetic.main.fragment_meu_canal.*
+import kotlinx.android.synthetic.main.fragment_meu_canal.view.*
 
 
 /**
@@ -23,14 +32,95 @@ import kotlin.collections.ArrayList
 class MeuCanalFragment : Fragment() {
 
 
+    private var listCursos = arrayListOf<Curso>()
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         val view: View = inflater.inflate(R.layout.fragment_meu_canal, container, false)
 
-
+        inicializar(view)
 
         return view
     }
 
+    fun inicializar(view: View) {
+
+        val viewDialog = ViewDialog(activity)
+        viewDialog.showDialog("Aguarde", "Obtendo informações de nossos servidores")
+
+        // Carrega as Informações do Usuário no Seu Perfil
+        val auth = FirebaseAuth.getInstance().currentUser
+        val usuario = FirebaseDatabase.getInstance().reference.child("usuarios").child(auth!!.uid)
+        val cursos = FirebaseDatabase.getInstance().reference.child("cursos")
+
+        usuario.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val u = dataSnapshot.getValue(Usuario::class.java)
+
+                if (u != null) {
+
+                    textNomeCanal.text = u.nome
+                    textDescricao.text = u.descricao
+
+                    if (u.urlPerfil.isNotEmpty()) {
+                        Glide.with(activity)
+                                .load(u.urlPerfil)
+                                .centerCrop()
+                                .into(imageProfile)
+                    }
+
+                    if (u.urlWallpaper.isNotEmpty()) {
+                        Glide.with(activity)
+                                .load(u.urlWallpaper)
+                                .centerCrop()
+                                .into(imageThumb)
+                    }
+                }
+
+
+                // Fecha o Dialog após carregar os dados
+                viewDialog.hideDialog()
+            }
+
+            override fun onCancelled(p0: DatabaseError) {
+
+            }
+        })
+
+        // Botão de Novo Curso
+        view.buttonNovoCurso.setOnClickListener {
+            startActivity(Intent(activity, NovoCursoActivity::class.java))
+        }
+
+        // Configura o RecyclerView de CursosDisponibilizados
+        val adapter = CursosAdapter(activity!!, listCursos)
+        var recyclerView = view.recyclerViewCursosDisponibilizados
+        recyclerView.layoutManager = LinearLayoutManager(activity)
+        recyclerView.adapter = adapter
+
+        cursos.addValueEventListener(
+                object : ValueEventListener {
+                    override fun onDataChange(dataSnapshot: DataSnapshot) {
+                        listCursos.clear()
+                        for (d in dataSnapshot.children) {
+                            val c = d.getValue(Curso::class.java)
+
+                            if (c?.idCanal.equals("${auth.uid}")) {
+                                listCursos.add(c!!)
+                            }
+                        }
+
+                        textNCursosDisponibilizados.text = listCursos.size.toString()
+                        adapter.notifyDataSetChanged()
+                        viewDialog.hideDialog()
+
+                    }
+
+                    override fun onCancelled(p0: DatabaseError) {
+
+                    }
+                })
+
+    }
 }
