9ac5b1275627c1593e6693e820ca062b20464a0e
Gladson <gladsonsouza@live.com>
2019-10-14 13:32:12 -0400

    Cartão adicionado (Model)

--- app/src/main/java/com/gmmp/easylearn/activity/PagamentoActivity.kt
+++ app/src/main/java/com/gmmp/easylearn/activity/PagamentoActivity.kt
@@ -3,7 +3,14 @@ package com.gmmp.easylearn.activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Toast
 import com.gmmp.easylearn.R
+import com.gmmp.easylearn.helper.cursoGlobal
+import com.gmmp.easylearn.helper.usuariosReferencia
+import com.gmmp.easylearn.model.Cartao
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.ValueEventListener
+import kotlinx.android.synthetic.main.activity_pagamento.*
 
 class PagamentoActivity : AppCompatActivity() {
 
@@ -14,6 +21,43 @@ class PagamentoActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true) //Mostrar o botão
         supportActionBar?.setHomeButtonEnabled(true)      //Ativar o botão
         supportActionBar?.title = "Cartão de Crédito"
+
+
+        val auth = FirebaseAuth.getInstance().currentUser?.uid.toString()
+
+        // CULPA DO PAULO | GLADSON
+//        usuariosReferencia().child(auth).child("cartoes").addValueEventListener(object : ValueEventListener {
+//
+//        })
+
+        button_confirmar.setOnClickListener {
+            if(text_numero_cartao.text.toString().isNotEmpty()){
+                if(text_nome_cartao.text.toString().isNotEmpty()){
+                    if(text_data_validade.text.toString().isNotEmpty()){
+                        if(text_codigo.text.toString().isNotEmpty()){
+                            var c = Cartao(text_numero_cartao.text.toString(), text_nome_cartao.text.toString(),
+                                    text_data_validade.text.toString(), text_codigo.text.toString())
+                            cadastrar(c)
+                        }else{
+                            Toast.makeText(this, "Erro: Preencha o código", Toast.LENGTH_SHORT).show()
+                        }
+                    }else{
+                        Toast.makeText(this, "Erro: Preencha a data de validade", Toast.LENGTH_SHORT).show()
+                    }
+                }else{
+                    Toast.makeText(this, "Erro: Preencha o nome do cartão", Toast.LENGTH_SHORT).show()
+                }
+            }else{
+                Toast.makeText(this, "Erro: Preencha o número do cartão", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
+    }
+
+    fun cadastrar(cartao: Cartao){
+        val auth = FirebaseAuth.getInstance().currentUser?.uid.toString()
+        usuariosReferencia().child(auth).child("cartao").setValue(cartao)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean { //Botão adicional na ToolBar
---
+++ app/src/main/java/com/gmmp/easylearn/model/Cartao.kt
@@ -0,0 +1,4 @@
+package com.gmmp.easylearn.model
+
+data class Cartao(val numeroCartao: String, val nomeCartao: String, val dataValidade: String, val codigo: String) {
+}
